e2c2d532aa6e38c8ef453beccde9fc9e1082c0c8
Pieter Michels <pieter.michels@gmail.com>
2010-09-19 12:20:46 +0200

    h

--- routes.rb
+++ routes.rb
@@ -41,7 +41,8 @@ get '/thumb/:id' do
   
   image_item = Photo.find(params[:id])
   image = image_item.thumb
-  
+  p image
+  p DPC
   if image.nil? && DPC.connect
     puts "Thumnbail :: Was not present, is saved now"
     
