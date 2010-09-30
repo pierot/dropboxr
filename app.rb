89316e0447c1ae06501b51125fca5d12eab4789d
Pieter Michels <pieter.michels@gmail.com>
2010-09-30 18:21:30 +0200

    trying to implement Failtale: #fail :(

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'less'
 require 'memcached'
 require 'timeout'
+#require "failtale-reporter"
 
 require File.dirname(__FILE__) + '/dropboxr.rb'
 
@@ -18,11 +19,17 @@ end
 
 configure do
   # Global constants
-  begin
-    CACHE = Memcached.new
-  rescue Memcached::Error
-    puts "Sinatra :: !! Memcache not available"
-  end
+  CACHE = Memcached.new
+  
+  #FailtaleReporter.configure do |config|
+  #  config.base_uri  '0.0.0.0'
+  #  config.api_token '405b0f2ceff8d8c3ff35df178b1c67a3fee77f8c'
+  #  config.application_root File.dirname(File.dirname(__FILE__))
+  #end
+  #
+  #FailtaleReporter.report do
+  #  raise 'Hello 2'
+  #end
   
   # Config files
   albums_excludes = YAML.load(File.read('config/excludes.yml'))
