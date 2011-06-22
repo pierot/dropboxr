8a885e93fb804566911440a6ddf488ca5769b4b9
Pieter Michels <pieter@noort.be>
2011-06-22 23:17:42 +0200

    ios fix ios only

--- public/js/base.js
+++ public/js/base.js
@@ -6,12 +6,18 @@ $(document).ready(function(){
 	
 });
 
-// iOS fix
-var a = document.getElementsByTagName("a");
+var isMobileDevice = function () {
+  return !!(navigator.userAgent.toLowerCase().match(/(iPhone|iPod|blackberry|android 0.5|htc|lg|midp|mmp|mobile|nokia|opera mini|palm|pocket|psp|sgh|smartphone|symbian|treo mini|Playstation Portable|SonyEricsson|Samsung|MobileExplorer|PalmSource|Benq|Windows Phone|Windows Mobile|IEMobile|Windows CE|Nintendo Wii)/i));
+};
 
-for(var i = 0; i < a.length; i++) {
-    a[i].onclick=function() {
-        window.location=this.getAttribute("href");
-        return false;
-    }
+// iOS fix
+if(isMobileDevice) {
+  var a = document.getElementsByTagName("a");
+
+  for(var i = 0; i < a.length; i++) {
+      a[i].onclick=function() {
+          window.location=this.getAttribute("href");
+          return false;
+      }
+  }
 }
