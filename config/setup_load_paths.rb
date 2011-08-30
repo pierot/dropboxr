bd957ef3fb8d1b1d8996eadc50d61ea905b28503
Pieter Michels <pieter@noort.be>
2011-08-30 16:43:16 +0200

    editing load_paths

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -2,9 +2,7 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
     rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
     rvm_lib_path = File.join(rvm_path, 'lib')
-
     $LOAD_PATH.unshift rvm_lib_path
-
     require 'rvm'
     RVM.use_from_path! File.dirname(File.dirname(__FILE__))
   rescue LoadError
@@ -12,7 +10,19 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
     raise "RVM ruby lib is currently unavailable."
   end
 end
- 
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
 
+# Select the correct item for which you use below.
+# If you're not using bundler, remove it completely.
+
+# If we're using a Bundler 1.0 beta
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'
+
+# Or Bundler 0.9...
+if File.exist?(".bundle/environment.rb")
+  require '.bundle/environment'
+else
+  require 'rubygems'
+  require 'bundler'
+  Bundler.setup
+end
