2fcdd7fc940dafa3b0ea284e942db9984d10be3b
Pieter Michels <pieter@noort.be>
2011-08-30 09:43:30 +0200

    changing deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,11 +12,12 @@ set :deploy_to,   '/srv/www/noort.be/domains/fotos2.noort.be/'
 
 role :web, domain
 role :app, domain
+role :db, domain, :primary => true
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-# set :rvm_ruby_string,       'ruby-1.9.2-head'
+set :rvm_ruby_string,       'ruby-1.9.2-p290@rails31'
 set :rvm_type,              :system
 set :use_sudo,              false
 set :group_writable,        true         # Shared environment
@@ -25,46 +26,49 @@ set :scm,                   :git
 set :deploy_via,            :remote_cache
 
 namespace :deploy do
-  # task :restart, :roles => :app, :except => { :no_release => true } do
-  #   run "#{sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
-  # end
-
-  after "deploy:finalize_update", "uploads:symlink"
-
-  after 'deploy:update_code' do
-    run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
   end
+
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "#{sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+
+  # after "deploy:finalize_update", "uploads:symlink"
+
+  # after 'deploy:update_code' do
+  #   run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+  # end
 
   on :start, "uploads:register_dirs"
 end
 
 namespace :uploads do
-  desc <<-EOD
-    Creates the upload folders unless the exist
-    and sets the proper upload permissions.
-  EOD
-  task :setup, :except => { :no_release => true } do
-    dirs = db_dirs.map { |d| File.join(shared_path, d) }
-    run "#{sudo} mkdir -p #{dirs.join(' ')} && #{sudo} chmod g+w #{dirs.join(' ')}"
-  end
+  # desc <<-EOD
+  #   Creates the upload folders unless the exist
+  #   and sets the proper upload permissions.
+  # EOD
+  # task :setup, :except => { :no_release => true } do
+  #   dirs = db_dirs.map { |d| File.join(shared_path, d) }
+  #   run "#{sudo} mkdir -p #{dirs.join(' ')} && #{sudo} chmod g+w #{dirs.join(' ')}"
+  # end
 
-  desc <<-EOD
-    [internal] Creates the symlink to uploads shared folder
-    for the most recently deployed version.
-  EOD
-  task :symlink, :except => { :no_release => true } do
-    run "rm -rf #{release_path}/db"
-    run "ln -nfs #{shared_path}/db #{release_path}/db"
-  end
+  #   [internal] Creates the symlink to uploads shared folder
+  #   for the most recently deployed version.
+  # EOD
+  # task :symlink, :except => { :no_release => true } do
+  #   run "rm -rf #{release_path}/db"
+  #   run "ln -nfs #{shared_path}/db #{release_path}/db"
+  # end
 
-  desc <<-EOD
-    [internal] Computes dirs and registers them
-    in Capistrano environment.
-  EOD
-  task :register_dirs do
-    set :db_dirs, %w(db db/data)
-    set :shared_children, fetch(:shared_children) + fetch(:db_dirs)
+  # desc <<-EOD
+  #   [internal] Computes dirs and registers them
+  #   in Capistrano environment.
+  # EOD
+  # task :register_dirs do
+  #   set :db_dirs, %w(db db/data)
+  #   set :shared_children, fetch(:shared_children) + fetch(:db_dirs)
 
-    uploads.setup
-  end
+  #   uploads.setup
+  # end
 end
