91d72ca7c7f0033c2925a072910bfebc3c472f00
Pieter Michels <pieter@noort.be>
2011-09-21 09:23:59 +0200

    testing new linode

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,7 @@ require 'capistrano_colors'
 set :application, 'dropboxr'
 set :repository,  'git@github.com:pierot/dropboxr.git'
 set :domain,      'tortuga'
-set :user,        'pirate'
-set :deploy_to,   '/srv/www/noort.be/domains/dropboxr.noort.be/'
+set :deploy_to,   '/srv/www/fotos.noort.be/'
 
 role :web, domain
 role :app, domain
@@ -17,7 +16,7 @@ role :db, domain, :primary => true
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-set :rvm_ruby_string,       'ruby-1.9.2-head@rails31'
+# set :rvm_ruby_string,       'ruby-1.9.2-head@rails31'
 set :rvm_type,              :system
 set :use_sudo,              false
 set :group_writable,        true         # Shared environment
@@ -35,7 +34,17 @@ namespace :deploy do
     run "#{sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
   end
 
-  after 'deploy:update_code' do
-    run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+  after "deploy:finalize_update", "db:symlink"
+
+  # after 'deploy:update_code' do
+  #   run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+  # end
+end
+
+namespace :db do
+
+  task :symlink, :except => { :no_release => true } do
+    run "mkdir -p #{shared_path}/db"
   end
+
 end
