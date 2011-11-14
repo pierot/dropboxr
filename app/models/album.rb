747ad917cf85b85dcc3f98c0a52aed367dfce174
Pieter Michels <pieter@noort.be>
2011-11-14 22:59:23 +0100

    more styling

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,8 +2,14 @@ class Album < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, :use => :slugged
 
+  scope :ordered, order('albums.name ASC')
+
   has_many :photos
 
   validates :name, :presence => true
   validates :path, :presence => true
+
+  def is_new?
+    Date.parse(modified).to_datetime.in_time_zone('UTC') > 1.month.ago
+  end
 end
