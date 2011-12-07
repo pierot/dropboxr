0dc4b855b8e575e3b00a9b27e897712749e42749
Pieter Michels <pieter@noort.be>
2011-12-07 22:40:16 +0100

    with resque status info in backend

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -5,6 +5,13 @@ class ManageController < ApplicationController
     @count_photos = Photo.all.length
     @count_cached_photos = Photo.cached.length
     @count_not_cached_photos = Photo.not_cached.length
+
+    info = Resque.info
+
+    @resque_pending = info[:pending]
+    @resque_working = info[:working]
+
+    # {:pending=>0, :processed=>904, :queues=>1, :workers=>2, :working=>0, :failed=>570, :servers=>["redis://localhost:6379/0"], :environment=>"development"}
   end
 
   protected
