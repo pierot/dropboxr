211152b2f2fc8766e5c3c29bfa7a94c29fae6e05
Pieter Michels <pieter@noort.be>
2012-07-16 14:49:27 +0200

    edit

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ActiveRecord::Base
   extend FriendlyId
+
   friendly_id :name, :use => :slugged
 
   scope :ordered, order('albums.name ASC').joins(:photos).group('photos.album_id').having('count(photos.album_id) > 0') # only galleries that have photos
